d74faa4fd838808bc815d8fe6d33756d7a40a696
Lucas Stuker <126526096+LucasStuker@users.noreply.github.com>
2023-09-10 22:21:43 -0300

    Initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { MovieService } from "../../api/movieService";
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+  async function getMovies() {
+    const {
+      data: { results },
+    } = await MovieService.getMovies();
+
+    setMovies(results);
+  }
+  useEffect(() => {
+    getMovies();
+  }, []);
+  useEffect(() => {
+    console.log(movies);}, [movies]);
+
+  
+  return (
+    <section className="Home">
+      {movies.map((movie) => (
+        <div>
+        {movie.id}
+        {movie.title}
+        </div>))}
+    </section>
+  );
+};
+
+export default Home;
